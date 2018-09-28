ce92bbddc69e5c79eb342ad0e2ff0cb13bddf876
Mike Potter <mpotter.drupal@gmail.com>
2018-09-28 16:12:15 -0400

    Initial outrigger support

---
+++ bin/init.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Causes this script to run inside the docksal cli container.
+#: exec_target = cli
+
+# Install Particle theme
+npx phase2/create-particle src/themes/particle
+
+# Install project requirements.
+composer clear-cache
+export COMPOSER_PROCESS_TIMEOUT=2000
+composer install
+
+# Install site
+drush si --db-url=mysql://user:user@db/drupal_octane minimal
+
+# Manually set username and password for the admin user.
+# @see https://github.com/acquia/blt/issues/2984.
+drush sql:query "UPDATE users_field_data SET name = 'admin' WHERE uid = 1;"
+drush user:password admin "admin"
